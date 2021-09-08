3ae76aa99750098dbf267af5f9d6207430cfda0d
Elena <rhapsodyenmauve@gmail.com>
2021-09-08 23:21:51 +0100

    function that adds new ask on the list

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// ---------------------------  add/hide delete button
 const tasks = document.querySelectorAll('.task');
 const deleteTasks = document.querySelectorAll('.delete-task');
 
@@ -10,3 +11,15 @@ function hideDelete() {
 
 tasks.forEach((task) => task.addEventListener('mouseover', showDelete));
 tasks.forEach((task) => task.addEventListener('mouseout', hideDelete));
+
+// ---------------------------  append new list item
+
+function appendListItem(task) {
+  const list = document.querySelector('ul');
+  const template = document.querySelector('#template-task');
+  const domFragment = template.content.cloneNode(true);
+
+  domFragment.querySelector('li p').textContent = task;
+  list.appendChild(domFragment);
+}
+appendListItem('Elena');
