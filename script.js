b7684c3c23b2aa23703efe227d9dcd2a4662bfdb
Elena <rhapsodyenmauve@gmail.com>
2021-09-09 14:54:01 +0100

    solved the DOM problem in this update, the DOM is re-scanned every time we add a new list element. So the functionalities keep working on every new li, since variables are re-assigned and event listeners are attached to them again. The variables that are affected and subject to change with every li element addition are declared with *var* in the script file instead of *const*.

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+const addBtn = document.querySelector('#add-icon');
+const form = document.querySelector('form');
+var tasks = document.querySelectorAll('.task');
+var deleteTasks = document.querySelectorAll('.delete-task');
+var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+const taskDescription = document.querySelector('#add-task');
+
 // ---------------------------  add/hide delete button
-const tasks = document.querySelectorAll('.task');
-const deleteTasks = document.querySelectorAll('.delete-task');
 
 function showDelete() {
   this.children[2].classList.remove('hide');
@@ -21,12 +26,29 @@ function appendListItem(task) {
 
   domFragment.querySelector('li p').textContent = task;
   list.appendChild(domFragment);
+
+  domUpdate();
+}
+
+// ---------------------------  DOM update
+// PAUSE HERE for a while...
+// with this function, I am re-scanning the DOM - we need to do this after the addition of a new list element through the usage of <template>
+// if we don't update the DOM, we cannot use the checkbox cross-out and delete button on new added elements
+
+function domUpdate() {
+  tasks = document.querySelectorAll('.task');
+  deleteTasks = document.querySelectorAll('.delete-task');
+  checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+  tasks.forEach((task) => task.addEventListener('mouseover', showDelete));
+  tasks.forEach((task) => task.addEventListener('mouseout', hideDelete));
+
+  checkboxes.forEach((checkbox) =>
+    checkbox.addEventListener('click', checkTask)
+  );
 }
 
 // ---------------------------  user adds new task
-const addBtn = document.querySelector('#add-icon');
-const form = document.querySelector('form');
-const taskDescription = document.querySelector('#add-task');
 
 function addTask(e) {
   e.preventDefault();
@@ -41,12 +63,12 @@ addBtn.addEventListener('click', addTask);
 
 function init() {
   taskDescription.value = '';
+  checkboxes.forEach((checkbox) => (checkbox.checked = false));
 }
 
 window.addEventListener('load', init);
 
 // ---------------------------  check task off
-const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
 function checkTask() {
   checkboxes.forEach((checkbox) => {
